db4f1310e0c4b51c774245248c81278e339ad67a
Max Pugh <mpugh@chapman.edu>
2026-01-29 11:56:33 -0800

    fixed quote

--- pages/mom-fights/chicken-fights/results.tsx
+++ pages/mom-fights/chicken-fights/results.tsx
@@ -19,7 +19,7 @@ export default function Results() {
             >
                 <img src="https://preview.redd.it/what-green-immortality-goo-was-lou-reed-drinking-and-can-we-v0-7urkojtxcyxd1.png?auto=webp&s=db15773f5cdff48871172b0c37d34be8879f6db0" alt="Results" />
                 <p className="italic text-center max-w-2xl">
-                   &quot;Based on discussions among fans, Lou Reed was associated with a "green immortality goo" during his later, health-conscious years, which likely referred to a homemade, nutrient-dense green juice consumed as part of his wellness routine. The exact, specific recipe is not widely documented, but such drinks commonly include leafy greens, cucumbers, and ginger.&quot;
+                   &quot;Based on discussions among fans, Lou Reed was associated with a 'green immortality goo' during his later, health-conscious years, which likely referred to a homemade, nutrient-dense green juice consumed as part of his wellness routine. The exact, specific recipe is not widely documented, but such drinks commonly include leafy greens, cucumbers, and ginger.&quot;
                 </p>
             </div>
 
